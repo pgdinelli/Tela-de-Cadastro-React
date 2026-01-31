9d7bca4b4af97e87e52de8e15e7bdbc5519f6cea
dinellipaulo <pgsd1453@gmail.com>
2026-01-31 14:15:00 -0400

    componente de conteúdo + refatoração do Main

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Tela de cadastro com React">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className='mb-0'>Este é um exemplo de sistema de cadastro usando React</p>
+    </Main>
