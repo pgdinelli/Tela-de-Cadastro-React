29e9acaa0d93cb92a46fe53ba4bc2ed52c4daab7
dinellipaulo <pgsd1453@gmail.com>
2026-01-31 13:26:17 -0400

    Implementação footer + navegação

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react';
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido por <a href="https://www.linkedin.com/in/paulodinelli/" target='_blank'>Paulo Dinelli</a>
+        </span>
     </footer>
