9bcb8e3b68e7626241b8dc4a97b4d9ae544c9c88
dinellipaulo <pgsd1453@gmail.com>
2026-02-02 15:41:53 -0400

    implementação das rotas de navegação com react-router-dom

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-             <NavItem href="#/" icon="home" title="Início"/>
-             <NavItem href="#/users" icon="users" title="Usuários"/>
+             <NavItem href="/" icon="home" title="Início"/>
+             <NavItem href="/users" icon="users" title="Usuários"/>
         </nav>
     </aside>
